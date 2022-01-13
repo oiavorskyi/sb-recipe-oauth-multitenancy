9e4cb43879d1834f3db8b0f15fa55931ca7cc3a7
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2022-01-13 15:08:46 -0500

    Add initial implementation

---
+++ src/main/java/com/jugglinhats/bootrecipes/oauthmultitenancy/OauthMultitenancyApplication.java
@@ -0,0 +1,13 @@
+package com.jugglinhats.bootrecipes.oauthmultitenancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthMultitenancyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthMultitenancyApplication.class, args);
+    }
+
+}
